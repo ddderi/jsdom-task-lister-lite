003829226b55e5b596dcde5175c568eb01c2ac14
ddderi <maxime830@live.fr>
2022-01-19 00:44:08 +0100

    commit tree 4246f9cdcd85ff35881e1532e9bcebc9bd40b0c7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+let forM = document.getElementById('create-task-form')
+
+
+// forM.addEventListener('submit', function(e){
+//    e.preventDefault();
+// })
