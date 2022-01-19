6b611c6a8f755a5e1df0d4a2c276a71177b16fc8
ddderi <maxime830@live.fr>
2022-01-19 23:08:37 +0100

    to do list done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+let newB = document.getElementsByTagName('button')
+let forM = document.getElementById('create-task-form')
+let taskk = document.getElementById('new-task-description')
+let ultask = document.getElementById('tasks')
+
+
+    
+
+// function bouton(){
+//   let newbtn = document.createElement('button')
+//   newbtn.innerHTML ='remove'
+//   document.newli.appendChild(newbtn)
+
+//  newbtn.onclick = function (){
+//    console.log('wowwwww')
+ // }
+// }
+
+
+function creaT(){
+let newli = document.createElement('li')
+   newli.appendChild(document.createTextNode(taskk.value))
+   ultask.appendChild(newli)
+  
+  let newbtn = document.createElement('button')
+  newbtn.innerHTML ='remove'
+  newbtn.className = 'btnclass'
+  newli.appendChild(newbtn)
+
+}
+
+
+
+
+forM.addEventListener('submit', function(e){
+   e.preventDefault();
+   creaT();
+  
+   
+   e.target.reset();
+})
+
+// function DLT(e){
+//   e.remove();
+// }
+
+//let btn = document.getElementsByClassName('btnclass')
+
+function showBtn(){
+  console.log('deded')
+}
+
+
+
+//document.addEventListener('click', function(e){
+  let btn = document.getElementsByClassName('btnclass')
+  for (var i = 0 ; i < btn.length; i++) {
+    btn[i].addEventListener('click' , showBtn())};
+  
+  
+   
+// I ll come back to this later 
